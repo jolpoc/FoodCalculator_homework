4203d3d84c37ac24cb337b2aa51d6181e51514e9
Jolanta Pockaja <jolantapockaja@gmail.com>
2020-08-10 19:12:59 +0300

    Made Food Calculator use 3 command arguments

--- project/FoodCalculator.scala
+++ project/FoodCalculator.scala
@@ -1,10 +1,9 @@
-class FoodCalculator extends App {
+object FoodCalculator extends App {
   val food = "potatoes"
   val price = 0.37
   val quantity = 20
-  val cost = price*quantity
+  val cost = price * quantity
   println(s"It will cost $cost to buy $quantity kilos of $food")
-
-
-
 }
+
+
